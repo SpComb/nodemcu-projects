ca9d6dcc362839d15b6e198e24c7e395f3c419a4
Tero Marttila <terom@fixme.fi>
2016-10-30 21:02:12 +0200

    dmx: led, sendCommand()

--- src/dmx.lua
+++ src/dmx.lua
@@ -1,11 +1,12 @@
 dmx = {
     uart_id   = 1,
-    gpio      = 2,
+    led_gpio  = 0,
 
     debug     = false,
 }
 
 function dmx.init()
+    gpio.mode(dmx.led_gpio, gpio.OUTPUT)
     uart.setup(dmx.uart_id, 250000, 8, uart.PARITY_NONE, uart.STOPBITS_2, 0)
 
     -- initial frame
@@ -16,9 +17,11 @@ end
 --
 -- XXX: first packet is ignored?
 function dmx.sendPacket(packet)
-    if dmx.debug then
-      print("dmx:sendPacket: ")
+    -- start
+    gpio.write(dmx.led_gpio, gpio.LOW)
+    print("dmx:sendPacket...")
 
+    if dmx.debug then
       for i = 1, #packet do
           print("\t" .. string.format("%02x", packet:byte(i)))
       end
@@ -31,22 +34,28 @@ function dmx.sendPacket(packet)
     -- packet
     uart.setup(dmx.uart_id, 250000, 8, uart.PARITY_NONE, uart.STOPBITS_2, 0)
     uart.write(dmx.uart_id, packet)
+
+    -- done
+    gpio.write(dmx.led_gpio, gpio.HIGH)
+    print("dmx:sendPacket done")
+end
+
+function dmx.sendCommand(command, data)
+  dmx.sendPacket(string.char(command) .. data)
 end
 
 -- Send repeated channel
 function dmx.sendValue(value, count)
-    local packet = string.char(0x00) .. string.rep(string.char(value), count)
-
-    dmx.sendPacket(packet)
+    dmx.sendCommand(0x00, string.rep(string.char(value), count))
 end
 
 -- Send table of channels
 function dmx.sendChannels(channels)
-    local packet = string.char(0x00)
+    local data = ""
 
     for i, chan in ipairs(channels) do
-        packet = packet .. string.char(chan)
+        data = data .. string.char(chan)
     end
 
-    dmx.sendPacket(packet)
+    dmx.sendCommand(0x00, data)
 end
