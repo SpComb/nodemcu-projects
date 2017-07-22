b5035ba15ddf7b3094cd8db22238ef91e50404ef
Tero Marttila <terom@fixme.fi>
2017-07-23 02:05:16 +0300

    adxl345: streaming fifo, d0/d1 deltas on trigger, detect +/- X/Y

--- test/adxl345.lua
+++ test/adxl345.lua
@@ -7,7 +7,7 @@ adxl345.write_u8(0x26, adxl345.time_inactive)      -- THRESH_ACT @ 1s/1
 adxl345.write_u8(0x27, 0xE0 + 0x0E) -- ACT_INACT_CTL ACT=dc-xy INACT=dc-xy
 adxl345.write_u8(0x31, 0x08 + 0x03) -- DATA_FORMAT FULL_RES=1 Range=16g
 adxl345.write_u8(0x2F, 0x00) -- INT_MAP Activity=INT1 Inactivity=INT1
-adxl345.write_u8(0x38, 0xC0 + 8) -- FIFO_CTL FIFO_MODE=Trigger Trigger=0 Samples=8
+adxl345.write_u8(0x38, 0x80 + 16) -- FIFO_CTL FIFO_MODE=Stream Trigger=0 Samples=16
 
 print(string.format("ADXL345 DEVID         %02x", adxl345.read_u8(0x00)))
 print(string.format("ADXL345 OFFSET        %+3d %+3d %+3d", adxl345.read_struct(0x1E, "<bbb")))
@@ -30,6 +30,7 @@ adxl345.write_u8(0x2E, 0x10) -- INT_ENABLE Activity !Inactivity
 print(string.format("ADXL345 POWER_CTL     %02x", adxl345.read_u8(0x2D)))
 print(string.format("ADXL345 INT_ENABLE    %02x", adxl345.read_u8(0x2E)))
 
+-- Print state, show absolute values
 function adxl345_print()
   local fifo_status = adxl345.read_u8(0x39)
   local fifo_trigger = bit.band(fifo_status, 0x80) ~= 0
@@ -51,6 +52,7 @@ function adxl345_print()
   print(string.format("ADXL345 INT: %02x", int_status))
 end
 
+-- Trigger on changes, show deltas
 function adxl345_trigger(event)
   local fifo_status = adxl345.read_u8(0x39)
   local fifo_trigger = bit.band(fifo_status, 0x80) ~= 0
@@ -58,46 +60,52 @@ function adxl345_trigger(event)
 
   print(string.format("ADXL345 @ %s: FIFO trigger=%s entries=%d ", event, tostring(fifo_trigger), fifo_entries))
 
-  local x1, y1, z1 = adxl345.read_xyz()
+  local x0, y0, z0 = adxl345.read_xyz()
 
-  print(string.format("ADXL345 @ %2d:  X=%+6d  Y=%+6d  Z=%+6d ", fifo_entries, x1, y1, z1))
+  print(string.format("ADXL345:       X=%+6d Y=%+6d Z=%+6d ", x0, y0, z0))
 
-  while fifo_entries > 1 do
-    local x2, y2, z2 = adxl345.read_xyz()
-    local dx = x2 - x1
-    local dy = y2 - y1
-    local dz = z2 - z1
-    x1 = x2
-    y1 = y2
-    z1 = z2
+  local x1 = x0
+  local y1 = y0
+  local z1 = z0
 
-    fifo_entries = fifo_entries - 1
+  for i = 1, fifo_entries do
+    local x, y, z = adxl345.read_xyz()
+    local dx0 = x - x0
+    local dx1 = x - x1
+    local dy0 = y - y0
+    local dy1 = y - y1
+    local dz0 = z - z0
+    local dz1 = z - z1
 
-    print(string.format("ADXL345 @ %2d: dX=%+6d dY=%+6d dZ=%+6d ", fifo_entries, dx, dy, dz))
-  end
+    if dx1 > adxl345.threshold_active * 16 then
+      print(string.format("ADXL345 @ %2d: +X %+6d          (d0 %+6d, d1 = %+6d)", i, x, dx0, dx1))
+    elseif dx1 < -adxl345.threshold_active * 16 then
+      print(string.format("ADXL345 @ %2d: -X %+6d          (d0 %+6d, d1 = %+6d)", i, x, dx0, dx1))
+    else
+      print(string.format("ADXL345 @ %2d:  X %+6d          (d0 %+6d, d1 = %+6d)", i, x, dx0, dx1))
+    end
 
-  if fifo_trigger then
-    -- reset trigger
-    adxl345.write_u8(0x38, 0) -- FIFO_CTL FIFO_MODE=Bypass Trigger=0 Samples=0
-    tmr.delay(5)
-    adxl345.write_u8(0x38, 0xC0 + 8) -- FIFO_CTL FIFO_MODE=Trigger Trigger=0 Samples=8
+    if dy1 > adxl345.threshold_active * 16 then
+      print(string.format("ADXL345 @ %2d:          +Y %+6d (d0 %+6d, d1 = %+6d)", i, y, dy0, dy1))
+    elseif dy1 < -adxl345.threshold_active * 16 then
+      print(string.format("ADXL345 @ %2d:          -Y %+6d (d0 %+6d, d1 = %+6d)", i, y, dy0, dy1))
+    else
+      print(string.format("ADXL345 @ %2d:           Y %+6d (d0 %+6d, d1 = %+6d)", i, y, dy0, dy1))
+    end
 
-    local fifo_ctl = adxl345.read_u8(0x38)
-    local fifo_status = adxl345.read_u8(0x39)
-    local fifo_trigger = bit.band(fifo_status, 0x80) ~= 0
-    local fifo_entries = bit.band(fifo_status, 0x3f)
-
-    print(string.format("ADXL345 reset trigger: FIFO ctl=%02x trigger=%s entries=%d ", fifo_ctl, tostring(fifo_trigger), fifo_entries))
+    x1 = x
+    y1 = y
+    z1 = z
   end
 
   -- clear interrupt
   local int_status = adxl345.read_u8(0x30)
 
-  print(string.format("ADXL345 INT: %02x", int_status))
+  print(string.format("ADXL345: INT %02x", int_status))
 end
 
 if false then
-    tmr.alarm(0, 1000, tmr.ALARM_AUTO, function(timer)
+  tmr.alarm(0, 1000, tmr.ALARM_AUTO, function(timer)
     adxl345_print()
   end)
 end
